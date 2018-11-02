1b129271ae43449081f3877c3f1fec176e1f285b
Dan Stafford <dan@Dans-MacBook-Pro.local>
2018-11-02 10:13:54 -0400

    Made whats_new a dependency of welcome, add number field to whats_new block, display on template

--- web/modules/custom/welcome/src/Controller/WelcomeController.php
+++ web/modules/custom/welcome/src/Controller/WelcomeController.php
@@ -3,6 +3,8 @@
 namespace Drupal\welcome\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Block\BlockManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class WelcomeController.
@@ -10,15 +12,43 @@ use Drupal\Core\Controller\ControllerBase;
 class WelcomeController extends ControllerBase {
 
   /**
+   * The block manager.
+   *
+   * @var \Drupal\Core\Block\BlockManagerInterface
+   */
+  protected $blockManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(BlockManagerInterface $block_manager) {
+    $this->blockManager = $block_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('plugin.manager.block')
+    );
+  }
+
+  /**
    * Welcome content.
    *
    * @return array
-   *   Return Welcome array.
+   *   Returns array of content to be passed to the welcome template.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
    */
   public function content() {
+    $whats_new_block = $this->blockManager->createInstance('whats_new_block', ['whats_new_number' => 2])->build();
+
     return [
       '#theme' => 'welcome',
       '#welcome_content' => $this->t('Welcome to welcome!'),
+      '#whats_new' => $whats_new_block,
     ];
   }
 
