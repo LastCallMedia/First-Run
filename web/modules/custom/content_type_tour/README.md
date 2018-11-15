a27b36cbdfd73c58ff6307212b840ac52badf02a
Dan Stafford <dan@Dans-MacBook-Pro.local>
2018-11-15 17:15:22 -0500

    Rename first_run_tours module to content_type_tour, adjust config too

---
+++ web/modules/custom/content_type_tour/README.md
@@ -0,0 +1,31 @@
+INTRODUCTION
+------------
+ 
+ Provides a quick method to create a tour for the /node/add/{content_type} page.
+
+   
+INSTRUCTIONS
+------------
+
+1. Visit the Manage Tour tab for a content-type: /admin/structure/types/manage/{content_type}/tour.
+
+2. Select the fields that should be included on the tour.
+
+3. Enable tour.
+
+4. You will be redirected to the tour UI (/admin/config/user-interface/tour/manage/{tour-id})where you can view the 
+content-type tour you just generated. Tips have been generated with the correct data-id and the tip body field has been 
+populated with the field helper text if it is present. On this tour UI you can further edit, add, and modify the tour 
+tips. See the tour_ui module documentation for more information.
+
+5. To auto-generate more tips based on content-type fields return to the Manage Tour tab and select additional fields. 
+Tour tips can only be deleted through the tour UI. 
+
+6. To disable a tour for a content type simply uncheck the enable tour checkbox on the Manage Tour UI.
+
+
+INSTALLATION
+------------
+
+Install as usual.
+
---
+++ web/modules/custom/content_type_tour/tests/src/Functional/ContentTypeTourFunctionalTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\Tests\content_type_tour\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Content Type Tour Functional tests.
+ */
+class ContentTypeTourFunctionalTest extends BrowserTestBase {
+
+  public static $modules = [
+    'system',
+    'node',
+    'tour',
+    'tour_ui',
+    'content_type_tour',
+  ];
+
+  /**
+   * Admin user account.
+   *
+   * @var \Drupal\user\Entity\User
+   */
+  protected $adminUser;
+
+  /**
+   * Machine name.
+   *
+   * @var string
+   */
+  protected $machineName;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $this->strictConfigSchema = FALSE;
+    parent::setUp();
+
+    $this->machineName = 'article';
+
+    $this->adminUser = $this->drupalCreateUser([
+      'administer content types',
+      'administer tour',
+      'access content',
+    ], NULL, TRUE);
+    $this->drupalLogin($this->adminUser);
+
+    $type = $this->container->get('entity_type.manager')->getStorage('node_type')
+      ->create([
+        'type' => $this->machineName,
+        'name' => 'Smarticle',
+      ]);
+    $type->save();
+    $this->container->get('router.builder')->rebuild();
+  }
+
+  /**
+   * TestContentTypeTourForm.
+   */
+  public function testContentTypeTourForm() {
+    $path = 'admin/structure/types/manage/' . $this->machineName . '/tour';
+    $this->drupalGet($path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('Tour Form');
+
+    $edit = [
+      'tour_enabled' => TRUE,
+    ];
+
+    $this->drupalPostForm($path, $edit, 'Save configuration', [], 'tour-form');
+
+    $this->drupalGet('admin/config/user-interface/tour');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('Tour');
+    $this->assertSession()->pageTextContains('node-add-' . $this->machineName);
+  }
+
+}
